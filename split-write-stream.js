558aa7cdcce7e945a0feb1678bf44e332be1e8d2
Arron <arronzhang@me.com>
2020-05-31 18:23:09 +0800

    Split write stream

---
+++ split-write-stream.js
@@ -0,0 +1,191 @@
+const path = require('path')
+const fs = require('fs')
+const assert = require('assert')
+const fsp = fs.promises
+
+class PartWriteStream extends fs.WriteStream {
+  _write(data, encoding, cb) {
+    super._write(data, encoding, err => {
+      if (!err) {
+        this.emit('writed', data)
+      }
+      cb(err)
+    })
+  }
+
+  _writev(data, cb) {
+    super._writev(data, err => {
+      if (!err) {
+        this.emit('writed', data)
+      }
+      cb(err)
+    })
+  }
+}
+
+class SplitWriteStream extends fs.WriteStream {
+  constructor(path, options = {}) {
+    assert(path, 'path required for check meta data')
+    assert(
+      typeof options.fileSize == 'number' && options.fileSize > 0,
+      'options.fileSize must bigger than zero for split'
+    )
+
+    const fileExists = fs.existsSync(path)
+    const metaPath = path + '.' + (options.metaExtname || 'sws')
+    const metaExists = fs.existsSync(metaPath)
+
+    super(path, options)
+    this.options = options
+    this.metaPath = metaPath
+
+    if (!metaExists && !fileExists) {
+      //Fist init meta.
+      this._meta = SplitWriteStream.buildMeta(this.options)
+      try {
+        this._updateMetaFile()
+      } catch (er) {
+        this._errOrDestory()
+      }
+    } else if (!metaExists && fileExists) {
+      //complate.
+      this._meta = {
+        fileSize: this.options.fileSize,
+        ranges: []
+      }
+    } else {
+      this._meta = this._readMetaFile()
+    }
+
+    this._checkEnd()
+
+    this._streams = []
+
+    this.once('open', () => {
+      if (!this.finished && this.fd) {
+        //check meta
+        try {
+          let stat = fs.fstatSync(this.fd)
+          if (stat.size === 0) {
+            fs.ftruncateSync(this.fd, this._meta.fileSize)
+          } else if (stat.size != this._meta.fileSize) {
+            //File changed.
+            this._errOrDestory(new Error(`File size does not match meta`))
+          }
+        } catch (er) {
+          this._errOrDestory(er)
+        }
+      }
+    })
+  }
+
+  _errOrDestory(er) {
+    if (er) {
+      if (this.autoClose) {
+        this.destroy()
+      }
+      this.emit('error', er)
+      return
+    }
+  }
+
+  static buildMeta({ fileSize, ranges, partSize, minPartSize, maxPart = 5 }) {
+    if (!ranges) {
+      ranges = []
+      let pos = 0
+      if (!partSize) {
+        partSize = Math.ceil(fileSize / maxPart)
+        if (partSize < minPartSize) {
+          partSize = minPartSize
+        }
+      }
+      let final = fileSize - 1
+      while (pos < fileSize) {
+        let end = pos + partSize - 1
+        end = Math.min(end, final)
+        ranges.push([pos, end])
+        pos = end + 1
+      }
+    }
+    return {
+      fileSize,
+      ranges
+    }
+  }
+
+  get meta() {
+    return this._meta
+  }
+
+  _readMetaFile() {
+    let data
+    try {
+      data = JSON.parse(fs.readFileSync(this.metaPath, { encoding: 'UTF-8' }))
+    } catch (err) {}
+    return data || SplitWriteStream.buildMeta(this.options)
+  }
+
+  _updateMetaFile() {
+    fs.writeFileSync(this.metaPath, JSON.stringify(this._meta, null, 2))
+  }
+
+  _unlinkMetaFile() {
+    try {
+      if (fs.existsSync(this.metaPath)) fs.unlinkSync(this.metaPath)
+    } catch (er) {}
+  }
+
+  _checkEnd() {
+    let unfinished = this.parts().find(p => !p.finished)
+    if (!unfinished) {
+      this.finished = true
+      this._unlinkMetaFile()
+      this.end()
+      return true
+    }
+    return false
+  }
+
+  _getRange(index) {
+    return this._meta && this._meta.ranges && this._meta.ranges[index]
+  }
+
+  unfinishParts() {
+    return this.parts().filter(p => !p.finished)
+  }
+
+  parts() {
+    return this._meta.ranges.map((r, index) => {
+      return {
+        index,
+        finished: r[0] > r[1],
+        range: r
+      }
+    })
+  }
+
+  partStream(index) {
+    if (!this._streams[index]) {
+      let range = this._getRange(index)
+      assert(range, 'Out of range')
+      const part = new PartWriteStream(this.path, {
+        fd: this.fd,
+        start: range[0],
+        autoClose: !this.fd,
+        parent: this
+      })
+      part.on('writed', () => {
+        let range = this._getRange(index)
+        range[0] = part.start + part.bytesWritten
+        try {
+          this._updateMetaFile()
+        } catch (er) {}
+        this._checkEnd()
+      })
+      this._streams[index] = part
+    }
+    return this._streams[index]
+  }
+}
+
+module.exports = SplitWriteStream
---
+++ split-write-stream.test.js
@@ -0,0 +1,156 @@
+const SplitWriteStream = require('./split-write-stream.js')
+const path = require('path')
+const fs = require('fs')
+const os = require('os')
+
+const testFile = path.join(os.tmpdir(), 'split-write-stream.data')
+const testMetaFile = testFile + '.sws'
+
+beforeEach(() => {
+  clean()
+})
+
+afterAll(() => {
+  clean()
+})
+
+function clean() {
+  if (fs.existsSync(testFile)) {
+    fs.unlinkSync(testFile)
+  }
+  if (fs.existsSync(testMetaFile)) {
+    fs.unlinkSync(testMetaFile)
+  }
+}
+
+describe('split write stream', () => {
+  test('build meta', () => {
+    const buildMeta = SplitWriteStream.buildMeta
+    expect(buildMeta({ fileSize: 100, ranges: [[0, 99]] })).toEqual({
+      fileSize: 100,
+      ranges: [[0, 99]]
+    })
+
+    expect(buildMeta({ fileSize: 100 })).toEqual({
+      fileSize: 100,
+      ranges: [
+        [0, 19],
+        [20, 39],
+        [40, 59],
+        [60, 79],
+        [80, 99]
+      ]
+    })
+    expect(buildMeta({ fileSize: 100, maxPart: 2 })).toEqual({
+      fileSize: 100,
+      ranges: [
+        [0, 49],
+        [50, 99]
+      ]
+    })
+
+    expect(buildMeta({ fileSize: 101, maxPart: 2 })).toEqual({
+      fileSize: 101,
+      ranges: [
+        [0, 50],
+        [51, 100]
+      ]
+    })
+
+    expect(buildMeta({ fileSize: 101, minPartSize: 100, maxPart: 2 })).toEqual({
+      fileSize: 101,
+      ranges: [
+        [0, 99],
+        [100, 100]
+      ]
+    })
+
+    expect(buildMeta({ fileSize: 101, partSize: 50 })).toEqual({
+      fileSize: 101,
+      ranges: [
+        [0, 49],
+        [50, 99],
+        [100, 100]
+      ]
+    })
+  })
+
+  test('part use fd', done => {
+    let stream = new SplitWriteStream(testFile, {
+      fileSize: 9,
+      maxPart: 3
+    })
+    stream.on('open', () => {
+      expect(stream.partStream(0)).toStrictEqual(stream.partStream(0))
+      stream.partStream(0).write(Buffer.from('abc'))
+      stream.partStream(0).end()
+      setTimeout(() => {
+        stream.partStream(1).write(Buffer.from('abc'))
+        stream.partStream(2).write(Buffer.from('abc'))
+      }, 100)
+    })
+    stream.on('finish', () => {
+      expect(fs.readFileSync(testFile, 'UTF-8')).toEqual('abcabcabc')
+      expect(fs.existsSync(testMetaFile)).toBeFalsy()
+      done()
+    })
+  })
+
+  test('part use path', done => {
+    let stream = new SplitWriteStream(testFile, {
+      fileSize: 9,
+      maxPart: 3
+    })
+
+    expect(stream.partStream(0)).toStrictEqual(stream.partStream(0))
+    stream.partStream(0).write(Buffer.from('abc'))
+    stream.partStream(0).end()
+    setTimeout(() => {
+      stream.partStream(1).write(Buffer.from('abc'))
+      stream.partStream(2).write(Buffer.from('abc'))
+    }, 100)
+
+    stream.on('finish', () => {
+      expect(fs.readFileSync(testFile, 'UTF-8')).toEqual('abcabcabc')
+      expect(fs.existsSync(testMetaFile)).toBeFalsy()
+      done()
+    })
+  })
+
+  test('reuse', done => {
+    let stream = new SplitWriteStream(testFile, {
+      fileSize: 9,
+      maxPart: 3
+    })
+    stream.on('open', () => {
+      let buf = fs.readFileSync(testFile)
+      expect(buf.length).toBe(9)
+      let part = stream.partStream(0)
+      part.write(Buffer.from('abc'))
+      part.end()
+      part.on('finish', () => {
+        stream.end()
+      })
+    })
+
+    stream.on('finish', () => {
+      let buf = fs.readFileSync(testFile)
+      expect(fs.existsSync(testMetaFile)).toBeTruthy()
+      let secStream = new SplitWriteStream(testFile, {
+        fileSize: 9
+      })
+
+      expect(secStream.unfinishParts()[0]).toHaveProperty('index', 1)
+
+      secStream.partStream(1).write('abc')
+      secStream.partStream(2).write('abc')
+      secStream.on('finish', () => {
+        let thirdStream = new SplitWriteStream(testFile, { fileSize: 9 })
+        expect(thirdStream.finished).toBeTruthy()
+        thirdStream.on('finish', () => {
+          done()
+        })
+      })
+    })
+  })
+})
